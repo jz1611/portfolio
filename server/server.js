30f10b365cd45f753362d5f20a29671beed7b496
Jeff Zivkovic <jeffzivkovic@Jeffs-MacBook-Pro.local>
2020-08-08 20:39:08 -0400

    Created some basic CSS styling

--- server/server.js
+++ server/server.js
@@ -5,18 +5,18 @@ const express = require('express');
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
 const app = express();
 
-// app.use(require('body-parser').text());
-// app.use(express.json());
+app.use(require('body-parser').text());
+app.use(express.json());
 
 MongoClient.connect(CONNECTION_STRING, {useUnifiedTopology: true})
     .then(client => {
         console.log('Connected to database.');
         app.set('db', client.db('portfolio-posts'));
-
-        app.get('/api/test', (req, res) => {
-            res.status(200).send('Test works')
-        });
-
-        app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}...`));
     })
     .catch(error => console.error(error));
+
+app.get('/api/test', (req, res) => {
+    res.status(200).send('Test works')
+});
+
+app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}...`));
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,7 +9,8 @@ function Home() {
             .get('/api/test')
             .then(res => {
                 console.log(res.data);
-            });
+            })
+            .catch(err => console.error(err));
     }, []);
 
     return (
