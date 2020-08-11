f75421a3014e2961b29f57bfb8c21c7f86d81e13
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-08-11 14:51:14 -0400

    HTML and CSS style changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,24 +5,26 @@ import './Header.css';
 
 function Header() {
     return (
-        <header>
-            <h1 id='header-name'>Jeff Zivkovic</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink exact to='/'>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/posts'>Posts</NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/contact'>Contact</NavLink>
-                    </li>
-                    <li>
-                        Resume
-                    </li>
-                </ul>
-            </nav>
+        <header id='page-header'>
+            <div id='header-inner'>
+                <nav id='header-nav-box'>
+                    <ul id='header-nav-list'>
+                        <li className='header-nav-item-box'>
+                            <NavLink exact to='/' className='header-nav-link'>Home</NavLink>
+                        </li>
+                        <li className='header-nav-item-box'>
+                            <NavLink exact to='/posts' className='header-nav-link'>Posts</NavLink>
+                        </li>
+                        <li className='header-nav-item-box'>
+                            <NavLink exact to='/contact' className='header-nav-link'>Contact</NavLink>
+                        </li>
+                        <li className='header-nav-item-box header-nav-link'>
+                            Resume
+                        </li>
+                    </ul>
+                </nav>
+                <h1 id='header-name'>Jeff Zivkovic</h1>
+            </div>
         </header>
     );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,8 +14,43 @@ function Home() {
     }, []);
 
     return (
-        <div>
-            Home
+        <div id='home-container'>
+            <div id='bio-container'>
+                <img alt='jeff zivkovic' />
+                <div>
+                    <h2>Hi there.</h2>
+                    <p>My name is Jeff Zivkovic and I am a software developer looking for my first job.</p>
+                </div>
+            </div>
+            <div>
+                <h2>Hard Skills</h2>
+                <div>
+                    <h3>Javascript</h3>
+                    <h3>HTML5</h3>
+                    <h3>CSS3</h3>
+                    <h3>React</h3>
+                    <h3>Node.js</h3>
+                    <h3>Express</h3>
+                    <h3>PostgreSQL</h3>
+                    <h3>Python</h3>
+                    <h3>MongoDB</h3>
+                </div>
+            </div>
+            <div>
+                <h2>Projects</h2>
+                <div>
+                    <img alt='man vs. potato' />
+                    <h3>Man vs. Potato</h3>
+                    <p>Javascript, React, HTML, CSS, etc.</p>
+                    <p>Summary goes here.</p>
+                </div>
+                <div>
+                    <img alt='chugging across america' />
+                    <h3>Chugging Across America</h3>
+                    <p>Javascript, React, HTML, CSS, etc.</p>
+                    <p>Summary goes here.</p>
+                </div>
+            </div>
         </div>
     );
 }
