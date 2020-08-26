fe74ce4734a8c29ad18a0d771c57993f6f5879ec
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-08-25 22:14:25 -0400

    Added contact page and initial styling

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,8 +4,11 @@ import './Contact.css';
 
 function Contact() {
     return (
-        <div>
-            Contact
+        <div id='contact-container'>
+            <p>Any questions or potential job prospects? Feel free to send me a message below.</p>
+            <address>
+                Email: <a href='mailto:jzivkovic1611@gmail.com'>Jeff Zivkovic</a>
+            </address>
         </div>
     );
 }
