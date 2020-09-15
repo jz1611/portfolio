64fbe8141beb410c487b96c3f43eb3c3b7074176
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-09-15 16:30:10 -0400

    finalizing drop down menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,21 @@
 // Dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const resume = require('./Zivkovic_Resume_Prof.pdf');
 
 function Header() {
+    const [menuStatus, setMenuStatus] = useState('closed');
+
+    const toggleMenu = () => {
+        if (menuStatus === 'closed') {
+            setMenuStatus('open');
+        } else {
+            setMenuStatus('closed')
+        }
+    }
+
     return (
         <header id='page-header'>
             <div id='header-inner'>
@@ -22,11 +32,22 @@ function Header() {
                         </li>
                     </ul>
                 </nav>
-                <menu id='header-menu'>
+                <menu id='hamburger' onClick={toggleMenu}>
                     Menu
                 </menu>
                 <h1 id='header-name'>Jeff Zivkovic</h1>
             </div>
+            <ul id='slide-menu' className={menuStatus}>
+                <li>
+                    <NavLink exact to='/' className='header-nav-link'>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink exact to='/contact' className='header-nav-link'>Contact</NavLink>
+                </li>
+                <li>
+                    <a className='header-nav-link' href={resume} rel='noopener noreferrer' target='_blank'>Resume</a>
+                </li>
+            </ul>
         </header>
     );
 }
