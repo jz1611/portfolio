58d12613c6af1ae8d29c47964e76e9ff04cb87c6
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-09-16 15:27:04 -0400

    getting ready to deploy

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path'); // Usually moved to the start of file
 const express = require('express');
 
 const { SERVER_PORT } = process.env;
@@ -6,5 +7,9 @@ const app = express();
 
 app.use(require('body-parser').text());
 app.use(express.json());
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}...`));
